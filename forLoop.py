da71252101826a4520c470bd245ff0e39d4e5b28
skwaweru254 <skwaweru254@gmail.com>
2022-11-15 09:59:01 +0300

    for loop in python

--- forLoop.py
+++ forLoop.py
@@ -1,5 +1,7 @@
 names = ["simon", "waweru", "254"]
 
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 for i in names:
     print(i)
     # iteration
@@ -7,3 +9,6 @@ for i in names:
 for i in range(3):
     print("Simon Waweru 254")
     # repetition
+
+for item in num:
+    print(item)
